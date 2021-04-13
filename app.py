3654e40516b8163d48125908a5b0e799b4c901e2
madhurendra1 <69914811+madhurendra1@users.noreply.github.com>
2021-04-13 11:18:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+@app.route('/', methods=['POST', 'GET'])
+def start():
+    return render_template('index.html')
+
+app.run(debug=True)
